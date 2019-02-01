9984e9de1a811a25dd180e0df69c3078000a1f09
blaisewang <kaito.kaito.k@gmail.com>
2019-02-01 15:58:13 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+USE: python <FILEPATH> (options)
+OPTIONS:
+    -h : print this help message and exit
+    -s : scp filepath
+    -t : text filepath
+    -o : output file prefix
+"""
+################################################################
+
+import getopt
+import sys
+
+################################################################
+# Command line options handling, and help
+
+opts, args = getopt.getopt(sys.argv[1:], 'hs:t:o:')
+opts = dict(opts)
+
+
+def print_help():
+    help_info = __doc__.replace('<FILEPATH>', sys.argv[0], 1)
+    print('-' * 60, help_info, '-' * 60, file=sys.stderr)
+    sys.exit()
+
+
+if '-h' in opts:
+    print_help()
+
+if len(args) > 0:
+    print("\n** ERROR: no arg files - only options! **\n", file=sys.stderr)
+    print_help()
+
+if '-s' not in opts:
+    print("\n** ERROR: must specify scp filepath (opt: -s) **\n", file=sys.stderr)
+    print_help()
+
+if '-t' not in opts:
+    print("\n** ERROR: must specify text filepath (opt: -t) **\n", file=sys.stderr)
+    print_help()
+
+if '-o' not in opts:
+    print("\n** ERROR: must specify output file prefix (opt: -o) **\n", file=sys.stderr)
+    print_help()
+
+with open(opts['-s']) as scp_file:
+    for line in scp_file:
+        print(line)
